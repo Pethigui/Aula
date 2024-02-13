2de2e250c1a72b2f2685e9c0414327689e77515d
Pethigui <pethigui8@gmail.com>
2024-02-13 17:48:40 -0300

    Correção de cores e outras coisas
    Nesse commit foi realizado a correção de cores e pequenos detalhes da tela de cadastro

--- cadastro.js
+++ cadastro.js
@@ -1,20 +1,22 @@
 function mudarElemento(ident){
+    document.getElementById(ident).style.color = "rgb(32, 138, 67)";
+    document.getElementById(ident).style.fontSize = "20px"
+}
+
+function voltarElemento(ident){
     document.getElementById(ident).style.color = "black";
     document.getElementById(ident).style.fontSize = "16px"
 }
 
-function voltarElemento(ident){
-    document.getElementById(ident).style.color = "rgb(26, 204, 64)";
-    document.getElementById(ident).style.fontSize = "12px"
-}
-
 
 function mudarBtn(){
-    document.getElementById('btnCadastro').style.color = "rgb(26, 204, 64)";
-    document.getElementById('btnCadastro').style.backgroundColor = "rgb(26, 204, 64)";
+    document.getElementById('nomebtn').style.color = "white";
+    document.getElementById('btnCadastro').style.backgroundColor = "black";
+    document.getElementById('nomebtn').style.fontSize = "20px";
 }
 
 function voltarBtn(){
-    document.getElementById('btnCadastro').style.color = "aliceblue";
-    document.getElementById('btnCadastro').style.backgroundColor = "rgba(71, 255, 108, 0.473)";
+    document.getElementById('nomebtn').style.color = "black";
+    document.getElementById('btnCadastro').style.backgroundColor = "rgb(32, 138, 67)";
+    document.getElementById('nomebtn').style.fontSize = "16px";
 }
